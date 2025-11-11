fbbd8e663d7e79f7c4f1bacc13d41b7411e6ea60
Anthony Kondratuk <akondratuk@plussimple.com>
2025-11-11 19:58:26 +0100

    feat: add GlobalSnackbar component for enhanced user notifications 🎉
    - Integrated GlobalSnackbar into the main App component to provide real-time feedback to users.

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import { store } from './src/store'
 import AppNavigator from './src/navigation/AppNavigator'
 import './src/i18n/i18n'
 import { PaperProvider } from 'react-native-paper'
+import { GlobalSnackbar } from './src/components/GlobalSnackBar'
 import BumbeezPaperTheme from './src/themes/paperTheme'
 
 export default function App() {
@@ -11,6 +12,7 @@ export default function App() {
         <Provider store={store}>
             <PaperProvider theme={BumbeezPaperTheme}>
                 <AppNavigator />
+                <GlobalSnackbar />
             </PaperProvider>
         </Provider>
     )
